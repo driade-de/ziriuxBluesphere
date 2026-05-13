a8bce780277d96251c828a6890ad908be7bc44fd
Hermes Antares <jose.cruz.bautista@gmail.com>
2026-05-12 21:13:34 -0600

    🚀 Forzar despliegue Ziriux

--- app/certificado/page.tsx
+++ app/certificado/page.tsx
@@ -1,11 +1,11 @@
-'use client'
-import { Suspense } from 'react'
+'use client' 
+import { Suspense } from 'react' 
 export default function CertificadoPage() {
   return (
-    <Suspense>
-      <div className="min-h-screen bg-black flex items-center justify-center text-emerald-500">
-        Nodo Certificado Ziriux: Sincronizado
-      </div>
-    </Suspense>
+    <Suspense> 
+      <div className="min-h-screen bg-black flex items-center justify-center text-emerald-500"> 
+        Nodo Certificado Ziriux: Sincronizado 
+      </div> 
+    </Suspense> 
   )
 }
